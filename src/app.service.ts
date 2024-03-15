80f78d124f7c7dab964e676d9b6c65f0ca970342
VinayakSoragavi <soragavivinayak@gmail.com>
2024-03-15 11:25:07 +0530

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const menu = {
+      name: "prakash patil",
+      age: 45
+    };
+    return JSON.stringify(menu);
   }
 }
