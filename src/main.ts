845ec25845f3afb73a8a8dd91af12939ba1ff0aa
VinayakSoragavi <soragavivinayak@gmail.com>
2024-05-20 16:32:59 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT||4000, '0.0.0.0');
+  await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('NestJS application failed to start', err);
+  process.exit(1);
+});
